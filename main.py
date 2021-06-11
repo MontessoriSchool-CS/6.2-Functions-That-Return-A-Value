c4421a20e8adf01323c5c7c88073234ff4142cf8
CompSciMrF <17910442+geekyteacher@users.noreply.github.com>
2021-06-11 12:38:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+# Task 1
+
+  # Add comments to explain what the output from this program will be and how you know.
+
+def maths1():
+  num1 = 50
+  num2 = 5
+  return num1 + num2
+
+def maths2():
+  num1 = 50
+  num2 = 5
+  return num1 - num2
+
+def maths3():
+  num1 = 50
+  num2 = 5
+  return num1 * num2
+
+outputNum = maths2()
+print(outputNum)
+
+# Task 2
+
+  # Adapt the code from one of the functions abover to create a new function called 'multiplier'.
+  # The user should be able to input two numbers that are stored in variables. 
+  # The function should multiply the two variables together and return the result to a variable in the main program.
+  # The main program should ouput the variable containing the result returned from the function.
